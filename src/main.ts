63b325aa99ed7b92be2630ac1f8637c417535a39
Julia Jakob <juliajakob2106@gmail.com>
2026-08-26 21:46:10 -0400

    Buttons for the piano

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div>Hi!</div>
+    <div class="piano">
+        <li><button class="lily">C</button></li>
+        <li><button class="log">C#</button></li>
+        <li><button class="lily">D</button></li>
+        <li><button class="log">Eb</button></li>
+        <li><button class="lily">E</button></li>
+        <li><button class="lily">F</button></li>
+        <li><button class="log">F#</button></li>
+        <li><button class="lily">G</button></li>
+        <li><button class="log">Ab</button></li>
+        <li><button class="lily">A</button></li>
+        <li><button class="log">Bb</button></li>
+        <li><button class="lily">B</button></li>
+        <li><button class="lily">C</button></li>
+    </div>
 `
